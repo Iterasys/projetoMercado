6c8990c86e4584da08145dc0f69045a7f64a6a67
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-10-13 21:08:49 -0300

    Inclui o Teste de Subtrair

--- __tests__/unit/calculadora.spec.js
+++ __tests__/unit/calculadora.spec.js
@@ -12,7 +12,7 @@ test("Somar 5 + 7", () => {
     // 1.2 Resultado Esperado
     const resultadoEsperado = 12;
 
-    // 2 - Executa
+    // 1.3 Configura a função que será testada
     // criando um objeto para receber a função da calculadora
     const somarDoisNumeros = calculadora.somarDoisNumeros;
     // executando a função somar dois numeros e guardando resultado
@@ -21,3 +21,20 @@ test("Somar 5 + 7", () => {
     // 3 - Valida
     expect(somarDoisNumeros(num1, num2)).toBe(resultadoEsperado);
 }) 
+
+test("Subtrair 15 - 7", () => {
+    // 1 - Configura / Arrange
+    // Entradas
+    const num1 = 15;
+    const num2 = 7;
+    // Saidas
+    resultadoEsperado = 8;
+
+    // 2 - Executa / Act
+    const subtrairDoisNumeros = calculadora.subtrairDoisNumeros;
+    const resultadoAtual = subtrairDoisNumeros(num1, num2);
+
+    // 3 - Valida / Assert
+    expect(resultadoAtual).toBe(resultadoEsperado);
+
+})
