a131d0b292ae49e9f6e9dab1e3603e2e43732afb
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-10-25 21:33:12 -0300

    Método Post da entidade Pet

--- __tests__/api/pet.spec.js
+++ __tests__/api/pet.spec.js
@@ -14,7 +14,21 @@ describe("PetStore Swagger - Pet", () => {
     // Post - teste de incluir um animal
     it("Post Pet", () => {
         // Configura
-        
+        // Apontou para o arquivo com os dados do animal
+        const jsonFile = require("../../vendors/json/pet1.json");
+        // Realizar a requisição e receber a resposta
+
+        // Executa
+        return request      // chamada para a requisição
+            .post("/pet")   // endpoint / função para incluir o animal
+            .send(jsonFile) // envia os dados no corpo da requisição
+            // Valida
+            .then((response) => {
+                assert.equal(response.statusCode, 200); // Se a requisição foi recebida e processada
+                assert.equal(response.body.id, petId);  // Se é o id esperado do animal
+                assert.equal(response.body.name, "Garfield"); // Se é o nome esperado
+                assert.equal(response.body.status, "available");   // Se está com o status esperado
+            });
     });
 });
 
