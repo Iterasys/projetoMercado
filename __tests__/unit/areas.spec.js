f09f2d1253934fd71fdb25bc726f0bbcf8a4508c
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-10-29 10:55:34 -0300

    Revisão: Testes de Unidade Data Driven

--- __tests__/unit/areas.spec.js
+++ __tests__/unit/areas.spec.js
@@ -1,6 +1,9 @@
 // aponte para o arquivo de desenvolvimento que deseja testar
 const areas = require("../../src/areas")
 
+// aponte para os arquivos de massa de teste
+const massaQuadrado = require("../../vendors/json/massaQuadrado")
+
 // funções de teste
 test("Area do Quadrado Simples - Lado Valido", () => {
     // Configura
@@ -34,6 +37,39 @@ test("Area do Quadrado Simples - Lado Nulo", () => {
     // Configura
     const lado = null
     const resultadoEsperado = "Por favor, informe um tamanho de lado válido"
+
+    // Executa
+    let calcularAreaDoQuadrado = areas.calcularAreaDoQuadrado
+    let resultadoAtual = calcularAreaDoQuadrado(lado)
+
+    // Valida
+    expect(resultadoAtual).toBe(resultadoEsperado)
+
+})
+
+let listaLados = [
+    [6, 36],
+    [0, "Por favor, informe um tamanho de lado válido"],
+    [-1, "Por favor, informe um tamanho de lado válido"],
+    [null, "Por favor, informe um tamanho de lado válido"]
+]
+
+test.each(listaLados)("Area do Quadrado Lendo Lista - Lado %f", (lado, resultadoEsperado) => {
+
+    // Executa
+    let calcularAreaDoQuadrado = areas.calcularAreaDoQuadrado
+    let resultadoAtual = calcularAreaDoQuadrado(lado)
+
+    // Valida
+    expect(resultadoAtual).toBe(resultadoEsperado)
+
+})
+
+test.each(massaQuadrado.array.map(elemento => [
+    elemento.lado,
+    elemento.resultadoEsperado
+]))
+("Area do Quadrado Lendo Massa - Lado %f", (lado, resultadoEsperado) => {
 
     // Executa
     let calcularAreaDoQuadrado = areas.calcularAreaDoQuadrado
---
+++ vendors/json/massaQuadrado.js
@@ -0,0 +1,22 @@
+module.exports.array = [
+    {
+      "lado": 7,
+      "resultadoEsperado": 49
+    },
+    {
+      "lado": 1000,
+      "resultadoEsperado": 1000000
+    },
+    {
+      "lado": 0,
+      "resultadoEsperado": "Por favor, informe um tamanho de lado válido"
+    },
+    {
+      "lado": -1,
+      "resultadoEsperado": "Por favor, informe um tamanho de lado válido"
+    },
+    {
+      "lado": null,
+      "resultadoEsperado": "Por favor, informe um tamanho de lado válido"
+    }
+  ]
